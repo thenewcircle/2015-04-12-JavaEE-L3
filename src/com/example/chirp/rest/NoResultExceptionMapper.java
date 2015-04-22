c5e06fbe8b8a2ec1c4683ad3a8b7c116eb539695
Doug Bateman <doug@dougbateman.net>
2015-04-22 14:12:05 -0400

    Adding MediaType to exception mapper.

--- src/com/example/chirp/rest/NoResultExceptionMapper.java
+++ src/com/example/chirp/rest/NoResultExceptionMapper.java
@@ -1,16 +1,20 @@
 package com.example.chirp.rest;
 
 import javax.persistence.NoResultException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class NoResultExceptionMapper implements ExceptionMapper<NoResultException> {
+public class NoResultExceptionMapper implements
+		ExceptionMapper<NoResultException> {
 
 	@Override
 	public Response toResponse(NoResultException ex) {
-		return Response.status(404).entity("Couldn't find that entity in database").build();
+		return Response.status(404)
+				.entity("Couldn't find that entity in database")
+				.type(MediaType.TEXT_PLAIN_TYPE).build();
 	}
 
 }
